0cb18a4649f3dc2c0db6aa8c03498192467d0f75
JARRIWON STIVEM MICAN CESPEDES <jarimices@gmail.com>
2023-01-04 21:09:18 -0500

    historias locas

---
+++ historias_locas.py
@@ -0,0 +1,6 @@
+adj=input("Adjetivo: ")
+verbo1=input("Verbo: ")
+verbo2=input("verbo:" )
+sustantivo_plural=input("sustantivo_plural: ")
+madlib=f"!Programar es tan {adj}!Siempre me emociona porque me encanta {verbo1} resolver prolemas. !aprende a {verbo2}con freecodcamp y alcaza tus {sustantivo_plural}!"
+print(madlib)
